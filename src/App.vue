87b9baa3ceddde9f83d619d47046064b28e0f37c
liqi.zhao <18363840809@163.com>
2018-10-23 08:52:34 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <ElTreeTransferDemo />
+  </div>
+</template>
+
+<script>
+import ElTreeTransferDemo from './components/ElTreeTransferDemo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ElTreeTransferDemo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
